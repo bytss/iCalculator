1865a416dcdd1e69c472f310846174b4ae561c58
bytss <jeffersonalaiza@yahoo.com>
2022-08-09 15:46:49 +0800

    update README.md

--- script.js
+++ script.js
@@ -8,12 +8,13 @@ const result = document.querySelector("#results")
 /*
  functions
 */
+
 function parseData(data){
     result.value += data
 }
 
 function calculate(){
-    results.value = math.evaluate(result.value)
+    results.value = math.evaluate(result.value) 
 }
 
 function clr(){
