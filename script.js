3c0b8bac1e0046e1ebf4281762596def5452f75a
bytss <jeffersonalaiza@yahoo.com>
2022-08-09 15:37:15 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+ @author matss
+*/
+
+//variables
+const result = document.querySelector("#results")
+
+/*
+ functions
+*/
+function parseData(data){
+    result.value += data
+}
+
+function calculate(){
+    results.value = math.evaluate(result.value)
+}
+
+function clr(){
+    result.value = ""
+}
